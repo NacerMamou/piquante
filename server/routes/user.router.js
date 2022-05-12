38144e450024a14cce070bcc0545f0b13f4198e5
Nacer Mamou <mamou.nacer@outlook.com>
2022-05-12 14:41:59 +0200

    deployment config on port 8009

---
+++ server/routes/user.router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const userRouter = express.Router();
+const { signUp, login }= require('../controllers/user.controller');
+
+userRouter.post('/signup', signUp);
+userRouter.post('/login', login);
+
+
+module.exports = userRouter;
